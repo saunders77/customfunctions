e329f9919ceb75a0087d6228668664a2f70add3d
Michael Saunders <michaelsaunders@outlook.com>
2017-10-17 13:54:14 -0700

    testing test add-in

--- test.js
+++ test.js
@@ -1,27 +1,110 @@
 
 Office.initialize = function(reason){
+    /*
+    // functions
+    function add42(num){
+        write("hello from beyond");
+        return num + 42000;
+    }
+
+    // definitions
+    Excel.Script.CustomFunctions["CF"] = {};
+    Excel.Script.CustomFunctions["CF"]["ADDTO44"] = {
+        call: add42,
+        description: "Returns the sum of a number and 42",
+        helpUrl: "https://example.com/help.html",
+        result: {
+            resultType: Excel.CustomFunctionValueType.number,
+            resultDimensionality: Excel.CustomFunctionDimensionality.scalar,
+        },
+        parameters: [{
+            name: "num",
+            description: "The number be added",
+            valueType: Excel.CustomFunctionValueType.number,
+            valueDimensionality: Excel.CustomFunctionDimensionality.scalar,
+        },],
+        options: {batch: false, stream: false,}
+    };
+
+    // registration
+    Excel.run(function (context) {
+        context.workbook.customFunctions.addAll();
+        return context.sync();
+    }).catch(function(error){
+        write("Error:" + error);
+    });
+    */
+    function write(myText){
+        var fullHTML = "";
+        Office.context.document.settings.refreshAsync(function(asyncResult){
+            if(Office.context.document.settings.get("console")){
+                fullHTML = Office.context.document.settings.get("console");
+            }
+            fullHTML += myText + "<br>";
+            Office.context.document.settings.set("console",fullHTML);
+            redrawConsole();
+            Office.context.document.settings.saveAsync(function(asyncResult){});
+        });
+    }
+    function redrawConsole(){
+        if(Office.context.document.settings.get("console")){
+            document.getElementById("debug").innerHTML = Office.context.document.settings.get("console");
+        }
+        else{
+            document.getElementById("debug").innerHTML = "";
+        }
+    }
+    function deleteConsole(){
+        Office.context.document.settings.set("console","");
+        redrawConsole();
+        Office.context.document.settings.saveAsync(function(asyncResult){});
+    }
+    document.getElementById("redrawConsole").onclick = redrawConsole;
+    document.getElementById("deleteConsole").onclick = deleteConsole;
+
 
     var debug = [];
     var debugUpdate = function(data){};
+    
+    /*
     function write(myText){
         debug.push([myText]);
         debugUpdate(debug);
         document.getElementById("debug").innerHTML += myText + "<br>";
     }
-
+    */
     function myDebug(setResult){
         debugUpdate = setResult;
     }
-
+/*
+    Excel.Script.CustomFunctions["CFDEBUG"] = {};
+    Excel.Script.CustomFunctions["CFDEBUG"]["DEBUG"] = {
+        call: myDebug,
+        description: "Outputs debug info to cells",
+        helpUrl: "https://example.com/help.html",
+        result: {
+            resultType: Excel.CustomFunctionValueType.string,
+            resultDimensionality: Excel.CustomFunctionDimensionality.matrix,
+        },
+        parameters: [
+        ],
+        options: {
+            batch: false,
+            stream: true,
+        }
+    };
+*/
     var mycode;
-    var needToRegister = true;
-    localStorage.setItem('readytoupdate', false);
+    var hiddenPane = true;
+    localStorage.setItem('readytoupdate', 0);
 
     if(!localStorage){
         write("no local storage");
     }
     else if(localStorage.getItem("mycode").length > 10){
         mycode = localStorage.getItem("mycode");
+        eval(mycode);
+        localStorage.setItem('readytoupdate', 0);
     }
     else{
         mycode = [
@@ -76,10 +159,10 @@ Office.initialize = function(reason){
     document.getElementById("run").onclick = function(){runcode();};
 
     function runcode(){
-        needToRegister = false;
+        hiddenPane = false;
         mycode = editor.getValue();
         localStorage.setItem('mycode', mycode);
-        localStorage.setItem('readytoupdate', true);
+        localStorage.setItem('readytoupdate', 1);
         eval(mycode);
         document.getElementById("load").innerHTML = "Loading...";
         setTimeout(function(){
@@ -92,23 +175,33 @@ Office.initialize = function(reason){
     // start checking when we need to run
     pingForUpdate();
     
+    //setInterval(messagePing, 3100);
 
     function pingForUpdate(){
+        write("Hidden? " + hiddenPane + "; Readytoupdate? " + localStorage.getItem('readytoupdate'));
+        
         if(!localStorage){
             write("no localstorage");
         }
         else{
-            if(needToRegister){
+            if(hiddenPane){
                 
-                if(localStorage.getItem('readytoupdate') === true){
+                if(localStorage.getItem('readytoupdate') == 1){
                     mycode = localStorage.getItem(mycode);
                     write("code changed " +localStorage.getItem('readytoupdate'));
                     eval(mycode);
-                    localStorage.setItem('readytoupdate', false);
+                    localStorage.setItem('readytoupdate', 0);
                 }
                 setTimeout(pingForUpdate,2000);
             } 
         }
+
+        
+        
+    }
+
+    function messagePing(){
+        write("am I hidden?" + hiddenPane);
         
     }
     
